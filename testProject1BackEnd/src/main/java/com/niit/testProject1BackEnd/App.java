0906587083036b9fb621c66fa63fa6ac8c8114b8
charan teja <charanteja.sri000001@gmail.com>
2017-12-04 12:38:16 +0530

    Front project  created succssfully

---
+++ testProject1BackEnd/src/main/java/com/niit/testProject1BackEnd/App.java
@@ -0,0 +1,13 @@
+package com.niit.testProject1BackEnd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
